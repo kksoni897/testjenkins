1531bd9f2afaa2731b245c16c3026001d241c7d3
kksoni897 <krishnakumarsoni897@gmail.com>
2018-06-07 10:42:52 +0530

    script change

--- starttestfile.sh
+++ starttestfile.sh
@@ -2,12 +2,12 @@
 if [ -e "pidfile" ]; then
    PID=`cat pidfile`
    if kill `cat pidfile`; then
-	while s=`ps -p $PID -o s=` && [[ "$s" && "$s" != 'Z' ]]; do sleep 1; done
-    	gunicorn -w 4 --threads 12 -b 0.0.0.0:9020 -p pidfile -D test:app
+        while s=`ps -p $PID -o s=` && [[ "$s" && "$s" != 'Z' ]]; do sleep 1; done
+        nohup gunicorn -w 4 --threads 12 -b 0.0.0.0:9020 -p pidfile test:app &
    fi
 else
-    gunicorn -w 4 --threads 12 -b 0.0.0.0:9020 -p pidfile -D test:app
+    nohup gunicorn -w 4 --threads 12 -b 0.0.0.0:9020 -p pidfile test:app &
 fi
-echo "process started successfully"
+
 
 
