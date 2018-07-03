8f2170431d746d843c63b1c997e968da10653ec0
kksoni897 <39952169+kksoni897@users.noreply.github.com>
2018-07-03 11:44:58 +0530

    Update starttestfile.sh

--- starttestfile.sh
+++ starttestfile.sh
@@ -1,13 +1,59 @@
 #!/bin/bash
+echo "***************Deployment Script Started************************"
+pathToSyntactic=$WORKSPACE
+. /home/krishna/softwares/anaconda3/bin/activate base
+hostPort=0.0.0.0:9020
+processToKillOnPort=[9]020
+processName=test:app
+echo "Anaconda environment setup is done at `pwd`"
+cd $pathToSyntactic
 if [ -e "pidfile" ]; then
    PID=`cat pidfile`
+   echo "old PID $PID"
    if kill `cat pidfile`; then
         while s=`ps -p $PID -o s=` && [[ "$s" && "$s" != 'Z' ]]; do sleep 1; done
-        nohup gunicorn -w 4 --threads 12 -b 0.0.0.0:9020 -p pidfile test:app &
-   fi
+        echo "Process killed using pidfile"
+   else
+        for pid in $( ps aux |ps -ef | grep $processToKillOnPort | awk '{print $2}');
+            do 
+                kill -9 $pid
+                while s=`ps -p $pid -o s=` && [[ "$s" && "$s" != 'Z' ]]; do sleep 1; done
+            done
+    fi
 else
-    nohup gunicorn -w 4 --threads 12 -b 0.0.0.0:9020 -p pidfile test:app &
+
+    for pid in $( ps aux |ps -ef | grep $processToKillOnPort | awk '{print $2}');
+        do 
+            kill -9 $pid
+            while s=`ps -p $pid -o s=` && [[ "$s" && "$s" != 'Z' ]]; do sleep 1; done
+        done
+        
 fi
-
-
-
+rm scriptOut.text
+nohup gunicorn -w 1 --threads=12 -b $hostPort -p pidfile $processName > scriptOut.text 2>&1 &
+pid=$!
+echo "new PID $pid"
+status=true
+time=0
+while $status
+    do
+        echo "checking api..."
+        response=$(curl -s -o /dev/null -w "%{http_code}\n" http://$hostPort/test --connect-timeout 2)
+        if [ $response -eq 200 ]; then
+            status=false
+        fi
+        echo "Response is : $response"
+        sleep 2
+        time=`expr $time + 2`
+        if [ $time -eq 10 ]; then
+            cat scriptOut.text
+            exit 1
+            break
+        fi
+    done
+cat scriptOut.text
+rm -rf PotentialNewOrchestrator
+rm -rf Structure-*
+cd $WORKSPACE
+cd ..
+rm -rf OrchestratorDeploy@tmp
