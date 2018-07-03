cf22a682d873f5a4896c515aa1640b5943a988e1
kksoni897 <krishnakumarsoni897@gmail.com>
2018-07-03 12:22:39 +0530

    test check

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
-from flask import Flask
-import gunicorn
+from flask import Flask,Response
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!!!!, krishna is here!"
+@app.route('/test')
+def test():
+    return Response(status=200)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=9020, threaded=True)
