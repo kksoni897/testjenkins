b442681383015bb630dd37c4e5b3f92b3ea9ca15
kksoni897 <39952169+kksoni897@users.noreply.github.com>
2018-07-04 17:13:57 +0530

    Update starttestfile.sh

--- starttestfile.sh
+++ starttestfile.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "***************Deployment Script Started************************"
-sudo cp mydir.tar /home/krishna/deploy/
+cp mydir.tar /home/krishna/deploy/
 cd /home/krishna/deploy/
-sudo tar -zxvf mydir.tar
+tar -zxvf mydir.tar
 cd /home/krishna/deploy/
 . /home/krishna/softwares/anaconda3/bin/activate base
 hostPort=0.0.0.0:9030
 processToKillOnPort=[9]030
 processName=test:app
 echo "Anaconda environment setup is done at `pwd`"
-sudo nohup gunicorn -w 1 --threads=12 -b $hostPort -p pidfile $processName > scriptOut 2>&1 &
+nohup gunicorn -w 1 --threads=12 -b $hostPort -p pidfile $processName > scriptOut 2>&1 &
 pid=$!
 echo "new PID $pid"
 status=true
