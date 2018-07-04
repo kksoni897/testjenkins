907708274b5eb47fc64238dea70c713f9e837d3e
kksoni897 <39952169+kksoni897@users.noreply.github.com>
2018-07-04 14:41:13 +0530

    Update starttestfile.sh

--- starttestfile.sh
+++ starttestfile.sh
@@ -2,7 +2,7 @@
 echo "***************Deployment Script Started************************"
 sudo cp mydir.tar /home/krishna/deploy/
 cd /home/krishna/deploy/
-tar -zxvf mydir.tar
+sudo tar -zxvf mydir.tar
 cd /home/krishna/deploy/
 . /home/krishna/softwares/anaconda3/bin/activate base
 hostPort=0.0.0.0:9020
